acfe745d5a8007378dc56dc0b6da2b440e5a99dd
MD HIDAYAT <55959880+Hidayat78@users.noreply.github.com>
2023-10-17 16:02:20 +0530

    Update Services.jsx
    just i change resume name resume to hdt

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -6,7 +6,7 @@ import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
 import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
-import Resume from './resume.pdf';
+import Resume from './hdt.pdf';
 
 const Services = () => {
   // context
