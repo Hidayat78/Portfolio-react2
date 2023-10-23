b1c1d6a0299f07bac7bee7338cf57b2be9b549bd
MD HIDAYAT <55959880+Hidayat78@users.noreply.github.com>
2023-10-23 14:47:45 +0530

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Card.css";
 
-const Card = ({emoji, heading, detail, color}) => {
+const Card = ({emoji,heading,detail,color}) => {
   return (
     <div className="card" style={{borderColor: {color}}}> 
       <img src={emoji} alt="" />
